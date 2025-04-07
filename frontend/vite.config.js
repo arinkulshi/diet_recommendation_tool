09ba448e27776b1a807a08817a27061aec08d4a1
arinkulshi <akulshi04@gmail.com>
2025-04-07 08:57:29 -0700

    added docker logic to work for all parts of the application

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,21 @@
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    watch: {
+      usePolling: true
+    },
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://api:3000',
         changeOrigin: true,
       },
     },
-  },
-});
+  }
+})
